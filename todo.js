c1bbee1c144e3c20a0b03fe94bbb58a63347a8ff
허우진 <heowoojin@heoujins-MacBook-Pro.local>
2020-04-02 02:45:45 +0900

    #4 delete clicked todo item from html element and local storage

--- todo.js
+++ todo.js
@@ -5,13 +5,41 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 const TODOS_LS = "toDos";
 const toDos = []; // 할 일 목록 저장할 배열
 
+function deleteLSToDo(id){
+    // 로컬db에 있는것도 지워야함.
+    let toDos = JSON.parse(localStorage.getItem(TODOS_LS));
+    for(key in toDos){
+        if(toDos[key].id == id){
+            console.log(toDos[key]);
+            toDos.splice(key, 1);
+        }
+    }
+    // 삭제한 todo항목을 다시 로컬db에 세팅해줌.
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+
+    // html요소에서 삭제
+    console.log(event.target.parentNode.id);
+    const delId = event.target.parentNode.id;
+    const delTarget = document.getElementById(delId);
+    console.log(delTarget);
+    delTarget.remove();
+
+    // 로컬db에서 삭제
+    deleteLSToDo(delId);
+}
+
+
+
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
+    if(loadedToDos !== null){
 
         const parsedToDos = JSON.parse(loadedToDos);
         // 배열에 있는 함수중에 하나인 forEach
@@ -29,6 +57,8 @@ function paintTodo(text){
     const newId = toDos.length+ 1;
 
     delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo);
+
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
